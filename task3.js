6ff26791661459dadf17ef811dfbdde47cf73c49
Tatyana Tyuniaeva <Tatyana5734478@yandex.ru>
2018-05-29 22:22:41 +0300

    task 3

---
+++ task3.js
@@ -0,0 +1,16 @@
+﻿var a = Math.abs(+process.argv[1]);
+var b = Math.abs(+process.argv[2]);
+
+greatestCommonDiviser(a, b)
+
+function greatestCommonDiviser (a, b) {
+    while (a != b && a > 0 && b > 0) {
+    	if (a > b) {
+        	a = a - b;
+    	} else {
+        	b = b - a;
+    	}
+	}
+
+	process.stdout.write("" + a);
+}
