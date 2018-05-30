2c459d5262260771f192738319edc47dd58557f9
Tatyana Tyuniaeva <Tatyana5734478@yandex.ru>
2018-05-30 22:40:37 +0300

    task3 other way

--- task3.js
+++ task3.js
@@ -1,16 +1,11 @@
 ﻿var a = Math.abs(+process.argv[1]);
 var b = Math.abs(+process.argv[2]);
 
-greatestCommonDiviser(a, b)
+process.stdout.write(String(greatestCommonDiviser(a, b)));
 
-function greatestCommonDiviser (a, b) {
-    while (a != b && a > 0 && b > 0) {
-        if (a > b) {
-            a = a - b;
-        } else {
-            b = b - a;
-        }
+function greatestCommonDiviser(a, b) {
+    if (b == 0) {
+        return Math.abs(a);
     }
-
-    process.stdout.write("" + a);
+    return greatestCommonDiviser(b, a % b);
 }
