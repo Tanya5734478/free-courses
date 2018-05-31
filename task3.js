7e1ffbbe1dc5dbf83abef288e73ba06caa623301
Tatyana Tyuniaeva <Tatyana5734478@yandex.ru>
2018-05-31 22:07:50 +0300

    task3 - return NaN

--- task3.js
+++ task3.js
@@ -4,7 +4,9 @@ var b = Math.abs(+process.argv[2]);
 process.stdout.write(String(greatestCommonDiviser(a, b)));
 
 function greatestCommonDiviser(a, b) {
-    if (b == 0) {
+    if (a == 0 && b == 0 || parseInt(a) != a || parseInt(b) != b){
+        return NaN;
+    } else if (b == 0) {
         return Math.abs(a);
     }
     return greatestCommonDiviser(b, a % b);
